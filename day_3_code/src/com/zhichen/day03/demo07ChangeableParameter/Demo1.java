371ddf96a6756669c73688d12ea80df11b8f6727
Zhi Chen <zhichen0310@163.com>
2020-08-12 17:07:41 +0800

    基础java首次提交

--- day_3_code/src/com/zhichen/day03/demo07ChangeableParameter/Demo1.java
+++ day_3_code/src/com/zhichen/day03/demo07ChangeableParameter/Demo1.java
@@ -14,15 +14,23 @@ package com.zhichen.day03.demo07ChangeableParameter;
  * 原理：
  *      可变参数底层是数组，根据传递参数个数不同，创建不同长度的数组，来存储这些参数。
  *      传递参数的个数可以0-多个
+ *
+ * 注意：
+ *      一个方法只能用一次可变参数
+ *      void method(int...a, String...b) //报错
+ *      如果有别的参数，可变参数放在末尾
+ *      void method(int...a, String b, double c)//报错
+ *      void method(String b, double c, int...a)//可以
  */
 public class Demo1 {
     public static void main(String[] args) {
-        add();
+        add(10,20);//arr.length=2,创建一个长度为2的数组，存储传递过来的参数new int[]{10,20}
     }
 
     //定义一个方法，计算n个int类型整数的和。参数个数不确定，我们使用可变参数
     public static int add(int...arr){
         System.out.println(arr);//[I@1b6d3586 数组的地址
+        System.out.println(arr.length);
         return 1;
 
     }
---
+++ day_3_code/src/com/zhichen/day03/demo08Collection/Notes.java
@@ -0,0 +1,45 @@
+package com.zhichen.day03.demo08Collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * @author Zhichen
+ * @school FZU
+ * @create 2020-08-12 16:55
+ *
+ *
+ * Collections  注意末尾有s，这是集合的常用工具类
+ * 常用方法:
+ *      public static <T> boolean addAll(Collection<T> c, T...elements)  往集合中添加一些元素
+ *      public static void shuffle(List<?> list)  打乱集合元素的顺序
+ *
+ *      public static <T> void sort(List<T> list)  将集合中元素按照默认规则排序  （默认升序）
+ */
+public class Notes {
+    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<>();
+//        Collections.addAll(list, "a","b","c","d","e");
+//        System.out.println(list);
+//        Collections.shuffle(list);
+//        System.out.println(list);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(0);
+        list.add(10);
+        list.add(7);
+        System.out.println(list);//[1, 2, 0, 10, 7]
+        Collections.sort(list);
+        System.out.println(list);//[0, 1, 2, 7, 10]
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("a");
+        list2.add("c");
+        list2.add("b");
+        System.out.println(list2);
+        Collections.sort(list2);
+        System.out.println(list2);
+    }
+}
