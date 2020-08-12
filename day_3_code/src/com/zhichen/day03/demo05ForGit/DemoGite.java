29031747459b18e2807ef660489d3fe7a6bd02af
Zhi Chen <zhichen0310@163.com>
2020-08-12 11:23:21 +0800

    基础java首次提交

---
+++ day_3_code/src/com/zhichen/day03/demo05ForGit/DemoGite.java
@@ -0,0 +1,12 @@
+package com.zhichen.day03.demo05ForGit;
+
+/**
+ * @author Zhichen
+ * @school FZU
+ * @create 2020-08-12 11:22
+ */
+public class DemoGite {
+    public static void main(String[] args) {
+        System.out.println("forGit测试");
+    }
+}
